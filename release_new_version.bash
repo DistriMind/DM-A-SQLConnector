aeb6152e2e2f32ace560974fd7a9d1619023c7af
Jason Mahdjoub <jason.mahdjoub@distri-mind.fr>
2025-05-16 10:04:54 +0200

    DM-A-SQLConnector 1.0.7-STABLE
    Signed-off-by: Jason Mahdjoub <jason.mahdjoub@distri-mind.fr>

---
+++ release_new_version.bash
@@ -0,0 +1,1 @@
+git add . && git commit -s -m "DM-A-SQLConnector $1" && git tag -s $1 -m "DM-A-SQLConnector $1" && git push && git push --tags && git push OneDev && git push OneDev --tags
--- src/test/java/fr/distrimind/oss/asqlconnector/ITest.java
+++ src/test/java/fr/distrimind/oss/asqlconnector/ITest.java
@@ -3,6 +3,7 @@ package fr.distrimind.oss.asqlconnector;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface ITest {
     void test(Connection conn) throws SQLException;
 }
